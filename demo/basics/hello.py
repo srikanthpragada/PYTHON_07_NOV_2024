63149c84e81f2d9f13dd08ce1c247fad44d3aa0d
srikanthpragada <srikanthpragada@gmail.com>
2024-11-11 17:44:20 +0530

    11-NOV

---
+++ demo/basics/hello.py
@@ -0,0 +1,4 @@
+# Take name and print hello
+
+name = input("Enter your name :")
+print("Hello", name)
---
+++ demo/basics/square.py
@@ -0,0 +1,7 @@
+# Take a number and print its square
+
+data = input("Enter a number :")
+num = int(data)    # convert str to int
+square = num * num
+
+print("Square =", square)
