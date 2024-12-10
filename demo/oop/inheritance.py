2c3920f75f9e5ea6296accc0cd472bb914d2e828
srikanthpragada <srikanthpragada@gmail.com>
2024-12-10 19:52:59 +0530

    10-DEC

--- demo/oop/inheritance.py
+++ demo/oop/inheritance.py
@@ -1,4 +1,8 @@
-class Doctor:
+from abc import ABC, abstractmethod
+
+
+# Abstract class
+class Doctor(ABC):
     def __init__(self, name, dept):
         self.name = name
         self.dept = dept
@@ -6,6 +10,11 @@ class Doctor:
     def show(self):
         print('Name  : ', self.name)
         print('Dept  : ', self.dept)
+
+    # Abstract method
+    @abstractmethod
+    def getsalary(self):
+        pass
 
 
 class ResidentDoctor(Doctor):
@@ -22,15 +31,26 @@ class ResidentDoctor(Doctor):
 
 
 class Consultant(Doctor):
-     def __init__(self, name, dept, visits, charge):
-         pass
+    def __init__(self, name, dept, visits, charge):
+        super().__init__(name, dept)
+        self.visits = visits
+        self.charge = charge
 
+    def show(self):
+        super().show()
+        print('Visits : ', self.visits)
+        print('Charge : ', self.charge)
+
+    def getsalary(self):
+        return self.visits * self.charge
+
+
+#d  = Doctor("a", "b")
 
 r = ResidentDoctor("Dr. James", "Card", 300000)
 r.show()
 print(r.getsalary())
 
 c = Consultant("Dr. Kim", "Gyn", 10, 1000)
-
-
-
+c.show()
+print(c.getsalary())
